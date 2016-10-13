af96d7a095902f5f7360357dcbdf1fd78945d63d
Hervé Ménager <hmenager@pasteur.fr>
2016-10-14 00:01:03 +0200

    correct the construction of the Galaxy link in tool description

--- bin/regate.py
+++ bin/regate.py
@@ -275,7 +275,7 @@ def build_general_dict(tool_meta_data, conf):
         u'description': format_description(tool_meta_data[u'description']),
         u'uses': [{
             "usesName": tool_meta_data[u'id'],
-            "usesHomepage": "{0}?id={1}".format(os.path.join('conf.galaxy_url', "root"),
+            "usesHomepage": "{0}?id={1}".format(conf.galaxy_url + '/root',
                                                       tool_meta_data[u'id']),
             "usesVersion": tool_meta_data[u'version']
         }],
